b33e4100951201e1f6b1c8074dddc46b5bf313fb
haunt <trunghau172837@gmail.com>
2024-09-15 15:27:17 +0700

    tesst

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user1', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
